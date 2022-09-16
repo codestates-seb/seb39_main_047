7906a83ac72f56f7c90a879fb132a47b7e467f36
devfrank9 <devfrank9@gmail.com>
2022-09-16 13:15:39 +0900

    ✨ Feat : api 함수 셋팅

--- client/src/apis/index.ts
+++ client/src/apis/index.ts
@@ -1,1 +1,39 @@
-export const hi = '';
+import axios from 'axios';
+
+const apiRoot = axios.create({
+  baseURL: 'http://localhost:8080',
+});
+
+export const getFreeBoards = async () => {
+  try {
+    const response = await axios.get('/v1/boards');
+    return response.data;
+  } catch (err) {
+    throw new Error();
+  }
+};
+
+export const postFreeBoard = async () => {
+  try {
+    return await axios.post('/v1/boards/create');
+  } catch (err) {
+    throw new Error();
+  }
+};
+
+export const getFreeBoard = async (id: number) => {
+  try {
+    const response = await axios.get(`/v1/boards/${id}`);
+    return response.data;
+  } catch (err) {
+    throw new Error();
+  }
+};
+
+export const removeFreeBoard = async (id: number) => {
+  try {
+    return await axios.delete(`/v1/boards/${id}`);
+  } catch (err) {
+    throw new Error();
+  }
+};

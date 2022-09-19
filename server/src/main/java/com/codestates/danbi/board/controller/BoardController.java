816f850c95109e0ce23e6f6d19deb35265489852
lcjne00 <lcjne00@gmail.com>
2022-09-19 11:55:52 +0900

    Feature: Swagger 2차적용
    Related to: #3

--- server/src/main/java/com/codestates/danbi/board/controller/BoardController.java
+++ server/src/main/java/com/codestates/danbi/board/controller/BoardController.java
@@ -7,6 +7,9 @@ import com.codestates.danbi.board.mapper.BoardMapper;
 import com.codestates.danbi.board.service.BoardService;
 import com.codestates.danbi.dto.MultiResponseDto;
 import com.codestates.danbi.dto.SingleResponseDto;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.Positive;
 import java.util.List;
 
+@ApiOperation(value = "게시판 API", tags = {"Board Controller"})
 @RestController
 @RequestMapping("/v1/boards")
 public class BoardController {
@@ -28,6 +32,12 @@ public class BoardController {
         this.mapper = mapper;
     }
 
+
+    @ApiOperation(value = "게시글 등록", notes = "게시글을 등록합니다.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "게시글 등록 완료"),
+            @ApiResponse(code = 404, message = "Board not found")
+    })
     @PostMapping("/create")
     public ResponseEntity postBoard(@Valid @RequestBody BoardPostDto boardPostDto) {
 
@@ -42,6 +52,8 @@ public class BoardController {
         return new ResponseEntity<>(new SingleResponseDto<>(response), HttpStatus.CREATED);
     }
 
+
+    @ApiOperation(value = "게시글 조회", notes = "게시글 식별자(boardId)에 해당하는 게시글을 조회합니다.")
     @GetMapping("/{board-id}")
     public ResponseEntity getBoard(@PathVariable("board-id") Long boardId) {
         Board board = boardService.findBoard(boardId);
@@ -52,11 +64,13 @@ public class BoardController {
     }
 
 
+    @ApiOperation(value = "게시글 수정", notes = "게시글 식별자(boardId)에 해당하는 게시글을 수정합니다.")
     @PatchMapping("/{board-id}")
     public ResponseEntity patchBoard(@PathVariable("board-id") @Positive Long boardId, @Valid @RequestBody BoardPostDto board) {
         return null;
     }
 
+    @ApiOperation(value = "게시글 전체 조회", notes = "page와 size에 맞게 게시글을 조회합니다.")
     @GetMapping
     public ResponseEntity findBoards(@RequestParam @Positive int page,
                                      @RequestParam @Positive int size) {
@@ -70,6 +84,8 @@ public class BoardController {
         return new ResponseEntity<>(new MultiResponseDto<>(responses,boardPage), HttpStatus.OK);
     }
 
+
+    @ApiOperation(value = "게시글 검색", notes = "page와 size에 맞게 검색어(keyword)에 해당하는 게시글을 검색합니다.")
     @GetMapping("/search")
     public ResponseEntity searchBoard(@RequestParam String keyword, @RequestParam @Positive int page,
                                       @RequestParam int size) {
@@ -82,6 +98,8 @@ public class BoardController {
         return new ResponseEntity<>(new MultiResponseDto<>(responses,boardPage),HttpStatus.OK);
     }
 
+
+    @ApiOperation(value = "게시글 삭제", notes = "게시글 식별자(boardId)에 해당하는 게시글을 삭제합니다.")
     @DeleteMapping("/{board-Id}")
     private ResponseEntity deleteBoard() {
         return null;
--- server/src/main/java/com/codestates/danbi/config/SwaggerConfiguration.java
+++ server/src/main/java/com/codestates/danbi/config/SwaggerConfiguration.java
@@ -26,7 +26,6 @@ public class SwaggerConfiguration {
     private ApiInfo swaggerInfo() {
         return new ApiInfoBuilder().title("Spring API Documentation")
                 .description("앱 개발시 사용되는 서버 API에 대한 연동 문서입니다.")
-                .license("pepega").licenseUrl("https://blog.naver.com/gudrb963")
                 .version("1")
                 .build();
     }

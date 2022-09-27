6bc852f4dc519deec4707c260196693311b681f7
Kyoorim <kyoorim88@gmail.com>
2022-09-27 16:44:11 +0900

    Feat: 서버연결 시 404 에러

--- client/src/components/LoginForm/index.tsx
+++ client/src/components/LoginForm/index.tsx
@@ -1,17 +1,52 @@
+import React, { useState } from 'react';
 import Button from '@components/Button';
 import Card from '@components/Card';
+import axios from 'axios';
 import * as S from './style';
 
 const LoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const response = await axios({
+      url: '/login',
+      method: 'post',
+      data: {
+        email: email,
+        password: password,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <Card>
-      <S.Form>
+      <S.Form onSubmit={submitHandler}>
         <S.Title>로그인</S.Title>
-        <S.Label>Email</S.Label>
-        <S.Input></S.Input>
-        <S.Label>Password</S.Label>
-        <S.Input></S.Input>
-        <Button type="submit">로그인 하기</Button>
+        <S.Label htmlFor="email">Email</S.Label>
+        <S.Input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></S.Input>
+        <S.Label htmlFor="password">Password</S.Label>
+        <S.Input
+          type="password"
+          id="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></S.Input>
+        <Button type="submit" onClick={() => {}}>
+          로그인 하기
+        </Button>
       </S.Form>
     </Card>
   );
--- client/src/components/SignupForm/index.tsx
+++ client/src/components/SignupForm/index.tsx
@@ -1,24 +1,65 @@
+import React, { useState } from 'react';
 import Card from '@components/Card';
 import Button from '@components/Button';
+import axios from 'axios';
 import * as S from './style';
 
 const SignupForm = () => {
+  const [nickname, setNickname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const submitHandler = async (event: React.FormEvent) => {
+    event.preventDefault();
+    const response = await axios
+      .post('/v1/members/join', {
+        username: nickname,
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <Card>
-      <S.Form>
+      <S.Form onSubmit={submitHandler}>
         <S.Title>회원가입</S.Title>
-        <S.Label>닉네임</S.Label>
-        <S.Input></S.Input>
-        <S.Label>이메일</S.Label>
+        <S.Label htmlFor="nickname">닉네임</S.Label>
+        <S.Input
+          type="nickname"
+          id="nickname"
+          value={nickname}
+          onChange={(e) => setNickname(e.target.value)}
+        ></S.Input>
+        <S.Label htmlFor="email">이메일</S.Label>
         <div>
-          <S.Input></S.Input>
-          <Button>중복확인</Button>
+          <S.Input
+            type="email"
+            id="email"
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></S.Input>
+          {/* <Button>중복확인</Button> */}
         </div>
-        <S.Label>비밀번호</S.Label>
-        <S.Input></S.Input>
-        <S.Label>비밀번호 확인</S.Label>
-        <S.Input></S.Input>
-        <Button type="submit">회원가입 하기</Button>
+        <S.Label htmlFor="password">비밀번호</S.Label>
+        <S.Input
+          type="password"
+          id="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></S.Input>
+        {/* <S.Label>비밀번호 확인</S.Label>
+        <S.Input></S.Input> */}
+        <Button type="submit" onClick={() => {}}>
+          회원가입 하기
+        </Button>
       </S.Form>
     </Card>
   );
